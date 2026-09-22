94fef75502289ef01cbdbdea53a35469438f07df
Falah <falahhereg@gmail.com>
2026-09-22 20:59:20 +0700

    fix: update frontend server port to 3000 in README and Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
     }
   },
   server: {
-    port: 5173,
+    host: '127.0.0.1',
+    port: 3000,
     open: true,
     proxy: {
       '/api': 'http://localhost:8000',
